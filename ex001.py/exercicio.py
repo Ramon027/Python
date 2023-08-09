5e740fd12bc1116cedb5c47834c5d078540eb11c
Ramon027 <129910859+Ramon027@users.noreply.github.com>
2023-08-08 21:30:27 -0300

    .

--- ex001.py/exercicio.py
+++ ex001.py/exercicio.py
@@ -1,33 +1,4 @@
-SP1
-
-#include <stdio.h>
-#include <stdlib.h>
-
-
-
-int main(int argc, char** argv) {
-    
-    char nome[30];
-    char endereco[30];
-    int idade;
-    
-    printf("digite seu nome: \n");
-    scanf("%s", &nome);
-    
-    printf("digite seu endereco\n");
-    scanf("%s", &endereco);
-    
-    printf("digite sua idade \n");
-    scanf("%d", &idade);
-    
-    printf("\n Nome: %s",nome );
-    
-    printf("\n Endereco: %s", endereco);
-    
-    printf("\n idade: %d", idade);
-
-    return (EXIT_SUCCESS);
-}
+dia = input ('Qual é o seu nome ? ')
 
 
 
@@ -46,30 +17,6 @@ int main(int argc, char** argv) {
 
 
 
-Media 
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char** argv) {
-    
-    float nota1;
-    
-    float nota2;
-    
-    float media;
-    
-    printf("Insira a nota 1:  ");
-    scanf("%f", &nota1);
-    
-    printf("Insira a nota 2: ");
-    scanf("%f", &nota2);
-    
-    media= (nota1 + nota2)/ 2; 
-    
-    printf("media = %f", media);
 
 
-    return (EXIT_SUCCESS);
-}
 
