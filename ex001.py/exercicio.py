b19955b4d175615b0cee5d911d60472e4601cb5b
Ramon027 <129910859+Ramon027@users.noreply.github.com>
2023-08-11 21:22:08 -0300

    Update exercicio.py

--- ex001.py/exercicio.py
+++ ex001.py/exercicio.py
@@ -1,4 +1,17 @@
-dia = input ('Qual é o seu nome ? ')
+a = input ('Digite algo:')
+print('O tipo primitivo desse valor é', type(a))
+print('Só tem espaços?', a.isspace())
+print('E um número?', a.isnumeric())
+print('E alfabético?', a.isalpha())
+print('E alfanumérico?', a.isalnum())
+print('Esta em maisculas?', a.isupper())
+print('Está em minusculas?', a.islower())
+print('Esta capitalizada?', a.istitle())
+
+
+
+
+
 
 
 
