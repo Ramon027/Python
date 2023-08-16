2c0df99ec9b9b7d9e25b750793b0951203612b80
Ramon027 <129910859+Ramon027@users.noreply.github.com>
2023-08-15 21:01:51 -0300

    .

---
+++ EX004/desafio2.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um numero:'))
+d = n *2
+t = n * 3
+r = n ** (1/2)
+print('Analisando o numero {} o seu dobro é {}, seu triplo é {}, e sua raiz quadrada é {:.2f}'.format(n, d, t, r))
