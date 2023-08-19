ca30f4f81a35cf85801adaebd6f4e0facc5250cf
Ramon027 <129910859+Ramon027@users.noreply.github.com>
2023-08-19 20:19:04 -0300

    at.

---
+++ EX004/desafio11.py
@@ -0,0 +1,5 @@
+dias = float(input('Por quantos dias o carro foi alugado ?'))
+km = float(input('Quantos km foram rodados ?'))
+pago = (dias * 60) + (km * 0.15)
+
+print('O total a pagar é de R${:.2f}'.format(pago))
