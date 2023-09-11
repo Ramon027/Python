bc6b66d15d185492f43001c034eae7a8c5fa40fb
Ramon027 <129910859+Ramon027@users.noreply.github.com>
2023-09-11 19:54:21 -0300

    at

---
+++ ex007/ex05.py
@@ -0,0 +1,8 @@
+d = float(input('Qual é a  distancia da sua viagem ?'))
+print('Você esta prestes a começar uma viagem de {}km.'.format(d))
+if d <= 200:
+    preço = d * 0.50
+
+else:
+    preço = d * 0.45
+print('E o preço da sua passagem será de R${:.2f}'.format(preço))  
